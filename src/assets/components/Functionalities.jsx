2a30875a9a802afe44b7e6006e1ea14ac6585c54
junfeng.lee <junfeng011995@gmail.com>
2024-07-31 09:11:16 +0800

    Included required attribute to text area

--- src/assets/components/Functionalities.jsx
+++ src/assets/components/Functionalities.jsx
@@ -59,6 +59,7 @@ const Functionalities = () => {
             className="bg-slate-50 appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline resize-none"
             rows="1"
             placeholder={`Enter functionality ${func.id}`}
+            required
           />
           <button
             type="button"
