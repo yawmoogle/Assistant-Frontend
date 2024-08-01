aab773f7c9520844d2d570f6bbf00edc99898f10
yawmoogle <102402826+yawmoogle@users.noreply.github.com>
2024-08-01 16:16:04 +0800

    Description in Details complete with test string in tooltip

--- src/assets/components/Details.jsx
+++ src/assets/components/Details.jsx
@@ -27,6 +27,7 @@ const Details = () => {
                 className="flex-grow p-2 border-none outline-none"
                 />
             </div>
+            <div>
             <label className="text-black font-bold mb-2 flex items-center mt-5">
                 Description
                 <span
@@ -34,15 +35,28 @@ const Details = () => {
                     onMouseEnter={() => setShowTooltip(true)}
                     onMouseLeave={() => setShowTooltip(false)}
                 >
-                i
+                <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-5 h-5"
+                >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 8H12.01M12 11V16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+              />
+            </svg>
                 {showTooltip && (
-                    <div className="absolute top-6 left-0 bg-grey-dark text-white text-xs rounded px-2 py-1">
-                        TOOLTIP HERE
+                    <div className="absolute bg-grey-dark text-white text-xs rounded px-2 py-1 z-50">
+                        This is a lengthy test string tooltip for the description in order to test the textbox wrapping and size. I am extending this test string until the end of the page to see if it fits or if it wraps.
                     </div>
                 )}
                 </span>
             </label>
-            <div className="border border-grey rounded-lg p-2 flex flex-wrap items-center focus-within:border-blue">
+            <div className="border border-grey rounded-lg p-2 flex flex-wrap items-center focus-within:border-blue mt-5">
             <input
                 type="text"
                 value={descriptionValue}
@@ -50,6 +64,7 @@ const Details = () => {
                 placeholder="Enter a short description of your project"
                 className="flex-grow p-2 border-none outline-none"
                 />
+        </div>
         </div>
         </div>
     );
