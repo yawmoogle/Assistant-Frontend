eee2fcf24bb626592e83c2e50e7ccc20a6354731
junfeng.lee <junfeng011995@gmail.com>
2024-08-05 15:34:16 +0800

    Redesign Add and Remove button for Functionalities

--- src/components/FuncRemoveButton.jsx
+++ src/components/FuncRemoveButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const RemoveButton = ({ onClick }) => {
+const RemoveButton = ({ onClick, className }) => {
   return (
     <button
       type="button"
       onClick={onClick}
-      className="bg-red-50 hover:bg-red-100 text-black font-bold text-m py-0.5 px-2 border-black rounded-full focus:outline-none focus:shadow-outline"
+      className={`absolute top-0.5 right-1 rounded-full ${className} cursor-pointer bg-slate-50 text-red-600 p-1 px-3 hover:bg-slate-100 `}
     >
       X
     </button>
