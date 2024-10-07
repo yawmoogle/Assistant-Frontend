649ea5e62c58c2731a140cad0627de754ffadc8f
yawmoogle <102402826+yawmoogle@users.noreply.github.com>
2024-10-07 16:17:59 +0800

    removed vulnerable sort code, switched css margins to use percentage for scalable formatting

--- src/components/home-page/outlets/WelcomeCard.jsx
+++ src/components/home-page/outlets/WelcomeCard.jsx
@@ -3,8 +3,8 @@ import "./welcome-card.css"
 const WelcomeCard = () => {
 
     return(<div className="welcome-card">
-        <h1 className="title">GenAI Project Name</h1>
-        <p className="description">Your Everyday AI Assistant</p>
+        <h1 className="text-white title">GenAI Project Name</h1>
+        <p className="text-white description">Your Everyday AI Assistant</p>
     </div>);
 };
 
