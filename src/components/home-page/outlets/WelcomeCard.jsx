a585b018b0be556daa94904364623c2b88bd9db5
yawmoogle <102402826+yawmoogle@users.noreply.github.com>
2024-11-25 14:18:45 +0800

    Welcome Card changes
    AIDEN icon added
    Sizing of welcome card to centralise

--- src/components/home-page/outlets/WelcomeCard.jsx
+++ src/components/home-page/outlets/WelcomeCard.jsx
@@ -3,8 +3,8 @@ import "./welcome-card.css"
 const WelcomeCard = () => {
 
     return(<div className="welcome-card">
-        <img src="/icon.png" alt="A logo of a few interconnected hexagons with AIDEN located on the left"/>
-        <p className="description">Your Everyday AI Assistant</p>
+        <img src="/icon_white.png" alt="A logo of a few interconnected hexagons with AIDEN located on the left"/>
+        <p className="description text-2xl">Your Everyday AI Assistant</p>
     </div>);
 };
 
