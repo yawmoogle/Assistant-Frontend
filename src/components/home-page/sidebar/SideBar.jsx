547cfafe26c254cc4f2660bec38b660012ff7567
yawmoogle <102402826+yawmoogle@users.noreply.github.com>
2024-11-27 09:41:27 +0800

    Sidebar text

--- src/components/home-page/sidebar/SideBar.jsx
+++ src/components/home-page/sidebar/SideBar.jsx
@@ -39,7 +39,7 @@ const SideBar = ({ showSidebar, sidebarRef }) => {
                     className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : "project"}
                   >
                     <ViewKanbanIcon />
-                    <p className="caption">{project.project_details.title}</p>
+                    <p className="text-black caption">{project.project_details.title}</p>
                   </NavLink>
                 </div>
             ))}
